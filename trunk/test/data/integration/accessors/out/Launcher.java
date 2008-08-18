338e2b4575034558ab37c0a2f2b33c62ebf2c1c3
slogther <slogther@db6b1310-884a-0410-9065-a97ea2a65816>
2008-08-18 15:38:03 +0000

    reszta

---
+++ trunk/test/data/integration/accessors/out/Launcher.java
@@ -0,0 +1,112 @@
+@interface Getter {
+}
+
+@interface Setter {
+}
+
+@interface Accessors {
+}
+
+class Adress {
+	String street;
+
+	int houseNo;
+}
+
+class PersonStub {
+	@Getter
+	@Setter
+	int x;
+
+	@Accessors
+	int y;
+
+	@Getter
+	String name;
+
+	@Setter
+	Adress adress;
+
+}
+
+public class Launcher {
+	static public void main(String[] args) {
+		Person person = new Person();
+		person.setY(7);
+		System.out.println(person.getY());
+	}
+}
+
+class AddAccessors extends metagenerics.runtime.MetaGeneric {
+	public metagenerics.ast.declarations.ClassDeclaration C;
+
+	public void setArgument(int i,
+			metagenerics.ast.declarations.ClassDeclaration arg) {
+		switch (i) {
+		case 1:
+			this.C = arg;
+			break;
+		default:
+			throw new metagenerics.exception.UnexpectedArgumentIndexException(i);
+		}
+	}
+
+	protected void translateMetaGenerics(
+			metagenerics.ast.metageneric.MetaTypedefAst typedef, StringBuilder result) {
+		{
+			for (metagenerics.ast.Node e : C.getChildren())
+				evaluate(e);
+
+			for (metagenerics.ast.member.Field m : C.getFields())
+				if (m.hasAnnotation("Getter") || m.hasAnnotation("Accessors"))
+					evaluate("%1$s get%2$s() {return %3$s;}", m.getType(),
+							util.StringUtils.capitalize(m.getName()), m
+									.getName());
+
+			for (metagenerics.ast.member.Field m : C.getFields())
+				if (m.hasAnnotation("Setter") || m.hasAnnotation("Accessors"))
+					evaluate("void set%2$s(%1$s arg) {%3$s = arg;}", m
+							.getType(), util.StringUtils
+							.capitalize(m.getName()), m.getName());
+		}
+	}
+}
+
+class Person {
+	@Getter
+	@Setter
+	int x;
+
+	@Accessors
+	int y;
+
+	@Getter
+	String name;
+
+	@Setter
+	Adress adress;
+
+	int getX() {
+		return x;
+	}
+
+	int getY() {
+		return y;
+	}
+
+	String getName() {
+		return name;
+	}
+
+	void setX(int arg) {
+		x = arg;
+	}
+
+	void setY(int arg) {
+		y = arg;
+	}
+
+	void setAdress(Adress arg) {
+		adress = arg;
+	}
+}
